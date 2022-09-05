f7e3ff83f237dcd10a46f4aa1534eac9f4220bdc
kevin caye <kevin.caye@probayes.com>
2022-09-05 11:15:47 +0200

    add pomodoro

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,8 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((org-attach-use-inheritance)
-     (org-roam-db-update-on-save))))
+   '((org-attach-use-inheritance . t)
+     (org-attach-use-inheritance)
+     (org-roam-db-update-on-save)))
+ '(warning-suppress-log-types '((org-element-cache) (org-element-cache) (defvaralias)))
+ '(warning-suppress-types '((org-element-cache) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
