5afbaf4163efabfb98ddfb3adaa84bc6fc380bd8
kevin caye <kevin.caye@probayes.com>
2022-09-15 16:09:06 +0200

    myhello and project

---
+++ .config/doom/config.d/project.el
@@ -0,0 +1,4 @@
+;;; ../../dotfiles2/.config/doom/config.d/project.el -*- lexical-binding: t; -*-
+
+;; TODO see https://docs.projectile.mx/projectile/usage.html
+(setq projectile-project-search-path '("~/dotfiles2/" "~/plain"))
