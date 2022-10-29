e9b48cf443e335a3adc11fea3875da2b7e815554
kevin caye <kevin.caye@probayes.com>
2022-10-29 14:37:52 +0200

    add projectile discover all project

--- .config/doom/config.d/project.el
+++ .config/doom/config.d/project.el
@@ -1,4 +1,7 @@
 ;;; ../../dotfiles2/.config/doom/config.d/project.el -*- lexical-binding: t; -*-
 
-;; TODO see https://docs.projectile.mx/projectile/usage.html
-(setq projectile-project-search-path '("~/dotfiles2/" "~/plain"))
+;; see https://docs.projectile.mx/projectile/usage.html
+(after! projectile
+  (setq projectile-project-search-path '("~/dotfiles2/" ("~/plain" . 4) ("~/projects" . 3)))
+  (projectile-discover-projects-in-search-path)
+  )
