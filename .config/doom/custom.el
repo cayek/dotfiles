b4944554def5c0cb820182babb6121d67cc419d7
Fanny et Kevin Caye <fk@caye.fr>
2023-01-27 09:31:00 +0100

    git-annex in cayek@cayator:~/dotfiles

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,7 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((ispell-dictionary . "fr")
+   '((eval add-hook 'after-save-hook
+      (lambda nil
+        (org-gfm-export-to-markdown))
+      nil t)
+     (ispell-dictionary . "fr")
      (org-src-preserve-indentation)
      (eval require 'ox-texinfo+ nil t)
      (eval require 'ol-info)
