1efb596dd0a6c2098e7ef8885f5bf1e70b5d716f
Fanny et Kevin Caye <fk@caye.fr>
2023-03-07 14:53:05 +0100

    add elgantt

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,30 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval add-hook 'after-save-hook
-      (lambda nil
-        (org-gfm-export-to-markdown))
-      nil t)
-     (ispell-dictionary . "fr")
-     (org-src-preserve-indentation)
-     (eval require 'ox-texinfo+ nil t)
-     (eval require 'ol-info)
-     (elisp-lint-indent-specs
-      (describe . 1)
-      (it . 1)
-      (thread-first . 0)
-      (cl-flet . 1)
-      (cl-flet* . 1)
-      (org-element-map . defun)
-      (org-roam-dolist-with-progress . 2)
-      (org-roam-with-temp-buffer . 1)
-      (org-with-point-at . 1)
-      (magit-insert-section . defun)
-      (magit-section-case . 0)
-      (org-roam-with-file . 2))
-     (elisp-lint-ignored-validators "byte-compile" "package-lint")
-     (org-roam-db-update-on-save))))
+ '(package-selected-packages '(emacsql-sqlite emacsql)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .config/doom/packages.d/common.el
+++ .config/doom/packages.d/common.el
@@ -18,6 +18,7 @@
 (package! ol-notmuch)
 (package! orgtbl-aggregate :recipe (:host github :repo "tbanel/orgaggregate"))
 (package! sphinx-doc)
+(package! elgantt :recipe (:host github :repo "legalnonsense/elgantt"))
 
 ;; (package! lsp-ltex :recipe (:host github
 ;;    :repo "emacs-languagetool/lsp-ltex"
