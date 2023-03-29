0000e91b481e28ab8eb1359d1f1446d8b4e69321
Fanny et Kevin Caye <fk@caye.fr>
2023-03-29 15:20:16 +0200

    add doom debug

---
+++ .emacs-profiles.el
@@ -0,0 +1,4 @@
+(("default" . ((user-emacs-directory . "~/.config/emacs")))
+ ("doom-debug"   (user-emacs-directory . "~/.config/emacs-debug")
+  (env ("DOOMDIR" . "~/.config/doom-debug")))
+ ("vanilla" . ((user-emacs-directory . "~/.config/vanilla-emacs"))))
